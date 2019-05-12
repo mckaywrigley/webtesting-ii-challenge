5352d5b09c80c93412253a3366276c5b78f14b56
Mckay <mckaywrigley@gmail.com>
2019-05-11 18:00:31 -0700

    sets up test file

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import * as rtl from "react-testing-library";
+import "jest-dom/extend-expect";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+afterEach(rtl.cleanup);
+
+it("renders without crashing", () => {
+  // ready to test!
 });
