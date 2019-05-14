58e54431ea75a0ded75d775f63058e9ce94b267b
Mckay <mckaywrigley@gmail.com>
2019-05-14 08:23:27 -1000

    tests Display component

--- client/src/Display.test.js
+++ client/src/Display.test.js
@@ -4,3 +4,17 @@ import { render, fireEvent } from "react-testing-library";
 import "react-testing-library/cleanup-after-each";
 
 import Display from "./Display";
+
+describe("<Display />", () => {
+  it("displays strikes", () => {
+    const { getByText } = render(<Display strikes={1} />);
+
+    getByText("Strikes: 1");
+  });
+
+  it("displays balls", () => {
+    const { getByText } = render(<Display balls={1} />);
+
+    getByText("Balls: 1");
+  });
+});
